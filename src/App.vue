efc5d6f9955d18544a9d9f0b4a7da32756cc1535
Lily <2542101862@qq.com>
2022-09-05 16:46:15 +0800

    aside响应式

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,9 @@
   export default {
     name: 'App',
     setup(){
-      const menuVisible = ref(false)
-      provide('menuVisible', menuVisible) // set
+      const width = document.documentElement.clientWidth;
+      const menuVisible = ref(width <= 500 ? false : true);
+      provide("menuVisible", menuVisible); // set
     }
   }
 </script>
