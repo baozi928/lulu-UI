68f360e48c53d31043b2a08623279aac8488f472
Lily <2542101862@qq.com>
2022-09-05 16:33:33 +0800

    Topnav封装

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,6 @@
 <template>
     <div>
-        <div class="topnav">
-            <div class="logo"></div>
-            <div class="menu"></div>
-        </div>
+        <Topnav/>
         <div class="banner">
             <h1>轱辘UI</h1>
             <h2>一个厉害的 UI 框架</h2>
@@ -14,7 +11,15 @@
         </div>
     </div>
 </template>
+
+<script lang="ts">
+    import Topnav from '../components/Topnav.vue'
+    export default {
+        components: {Topnav}
+    }
+</script>
+
 <style lang="scss" scoped>
-    .topnav{border: 1px solid red;}
+
 
 </style>
