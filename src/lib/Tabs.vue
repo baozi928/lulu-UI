d1b2252d9caf55ef18dc5c602679567c4965be01
Lily <2542101862@qq.com>
2022-09-08 15:43:34 +0800

    Tabs 1页面

---
+++ src/lib/Tabs.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="lulu-tabs">
+        <div class="lulu-tabs-nav">
+            <div class="lulu-tabs-nav-item" v-for="(t,index) in titles" @click="select(t)" :class="{selected: t=== selected}" :key="index">{{t}}</div>
+        </div>
+        <div class="lulu-tabs-content">
+            {{current}}
+            <component class="lulu-tabs-content-item" :is="current" />
+    </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import Tab from './Tab.vue'
+    import {
+        computed
+    } from 'vue'
+    export default {
+        props: {
+            selected: {
+                type: String
+            }
+        },
+        setup(props, context) {
+            const defaults = context.slots.default()
+            defaults.forEach((tag) => {
+                if (tag.type !== Tab) {
+                    throw new Error('Tabs 子标签必须是 Tab')
+                }
+            })
+            const current = computed(() => {
+                console.log('重新 return')
+                return defaults.filter((tag) => {
+                    return tag.props.title === props.selected
+                })[0]
+            })
+            const titles = defaults.map((tag) => {
+                return tag.props.title
+            })
+            const select = (title: string) => {
+                context.emit('update:selected', title)
+            }
+            return {
+                defaults,
+                titles,
+                select,
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    $blue: #40a9ff;
+    $color: #333;
+    $border-color: #d9d9d9;
+    .lulu-tabs {
+        &-nav {
+            display: flex;
+            color: $color;
+            border-bottom: 1px solid $border-color;
+            &-item {
+                padding: 8px 0;
+                margin: 0 16px;
+                cursor: pointer;
+                &:first-child {
+                    margin-left: 0;
+                }
+                &.selected {
+                    color: $blue;
+                }
+            }
+        }
+        &-content {
+            padding: 8px 0;
+        }
+    }
+</style>
