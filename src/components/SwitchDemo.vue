2f73dea704c5c496fea3da14b702ff4f45139ce4
Lily <2542101862@qq.com>
2022-09-10 03:02:40 +0800

    高亮代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -4,52 +4,58 @@
         <div class="demo">
             <h2>常规用法</h2>
             <div class="demo-component">
-                <Switch1Demo />
+                <component :is="Switch1Demo"/>
             </div>
             <div class="demo-actions">
                 <Button>查看代码</Button>
             </div>
             <div class="demo-code">
-                <pre>{{Switch1Demo.__sourceCode}}</pre>
+                <pre class="language-html"
+                     v-html="Prism.highlight(Switch1Demo.__sourceCode, Prism.languages.html, 'html')"/>
             </div>
-    </div>
+        </div>
         <div class="demo">
             <h2>支持 disabled </h2>
             <div class="demo-component">
-                <Switch2Demo />
+                <component :is="Switch2Demo"/>
             </div>
             <div class="demo-actions">
                 <Button>查看代码</Button>
             </div>
             <div class="demo-code">
-                <pre>>{{Switch2Demo.__sourceCode}}</pre>
+                <pre class="language-html"
+                     v-html="Prism.highlight(Switch2Demo.__sourceCode, Prism.languages.html, 'html')"/>
             </div>
         </div>
     </div>
 </template>
 
 <script lang="ts">
-    import Switch from '../lib/Switch.vue'
-    import Button from '../lib/Button.vue'
-    import Switch1Demo from './Switch1.demo.vue'
-    import Switch2Demo from './Switch2.demo.vue'
-    import { ref } from 'vue'
+    import Switch from '../lib/Switch.vue';
+    import Button from '../lib/Button.vue';
+    import Switch1Demo from './Switch1.demo.vue';
+    import Switch2Demo from './Switch2.demo.vue';
+    import 'prismjs';
+    import 'prismjs/themes/prism.css';
+
+    import {
+        ref
+    } from 'vue';
+    const Prism = (window as any).Prism;
     export default {
         components: {
-            Switch1Demo,
-            Switch2Demo,
-            Switch,
             Button
         },
         setup() {
-            const bool = ref(false)
+            const bool = ref(false);
             return {
                 bool,
                 Switch1Demo,
-                Switch2Demo
-            }
+                Switch2Demo,
+                Prism,
+            };
         }
-    }
+    };
 </script>
 
 <style lang="scss" scoped>
@@ -85,4 +91,4 @@
         }
     }
 
-    </style>
+</style>
