19f123fffcb8a44b97aa0e95914d8cc5db1028a7
Lily <2542101862@qq.com>
2022-09-12 14:17:19 +0800

    小小优化2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -56,12 +56,22 @@
     }
     .features {
         margin: 64px auto;
-        width: 400px;
+        padding: 0 16px;
         @media (min-width: 800px) {
             width: 800px;
+            >ul {
+                >li {
+                    width: 50%;
+                }
+            }
         }
         @media (min-width: 1200px) {
             width: 1200px;
+            >ul {
+                >li {
+                    width: 33.3333%;
+                }
+            }
         }
 
         > ul {
