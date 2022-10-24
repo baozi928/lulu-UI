2e16e2b82aa2024896a84c957ec87d8e8aa828a6
Lily <2542101862@qq.com>
2022-10-24 14:05:41 +0800

    添加Progress1

---
+++ src/components/Progress.demo.vue
@@ -0,0 +1,30 @@
+<demo>
+    Progress示例
+</demo>
+<template>
+    <div class="g-container">
+        <div class="g-progress"></div>
+    </div>
+
+</template>
+
+<script lang="ts">
+    export default {
+        name: 'Progress'
+    }
+</script>
+
+<style>
+    .g-container {
+        width: 240px;
+        height: 25px;
+        border-radius: 25px;
+        background: #eee;
+    }
+    .g-progress {
+        width: 50%;
+        height: inherit;
+        border-radius: 25px 0 0 25px;
+        background: #72bcd5;
+    }
+</style>
---
+++ src/components/ProgressDemo.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>Progress 示例</h1>
+    <Demo :component="ProgressDemo" />
+</template>
+
+<script lang="ts">
+    import Demo from './Demo.vue'
+    import ProgressDemo from './Progress.demo.vue'
+    import {
+        ref
+    } from 'vue'
+    export default {
+        components: {
+            Demo
+        },
+        setup() {
+            return {
+                ProgressDemo
+            }
+        }
+    }
+
+</script>
